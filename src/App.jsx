0ffabf9148225b4f0a516aeca7835e2e18c0c502
Ali Joshany <aa6joshany@gmail.com>
2025-07-06 09:08:58 +0330

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <div className="main">
         <h1>We Are Comming Soon</h1>
         <img className="first-img"
-          src="src/assets/1.jpg"
+          src="/src/assets/1.jpg"
           alt="Ali Joshany"
           width="auto"
           height="100%"
