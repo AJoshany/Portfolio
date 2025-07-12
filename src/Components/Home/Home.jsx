989c87fa21e705ab250203058c476329188bc7dc
Ali Joshany <aa6joshany@gmail.com>
2025-07-12 17:44:36 +0330

    Home Changed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,16 @@ import './Home.css'
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className="home--main">
+            <div className='home__left'>
+                <h1 className='home--heading'>Ali Joshany</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium sunt ad suscipit tenetur, ab incidunt consequatur omnis placeat quaerat similique.</p>
+                <button className='home--btn'><a href='/projects'>Projects</a></button>
+            </div>
+
+            <div>
+                <img src="/public/images/home.jpg" alt="Ali Joshany" width="500px" className='home--img' />
+            </div>
+        </div>
     )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-import "./Navbar.css"
+import './Navbar.css'
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <>
+            <nav className='navbar-container'>
+                <div className='navbar-left'>
+                    <ul className='navbar-list'>
+                        <li className="navbar-item active"><a href='#'>Home</a></li>
+                        <li className="navbar-item"><a href='#'>Skills</a></li>
+                        <li className="navbar-item"><a href='#'>Projects</a></li>
+                        <li className="navbar-item"><a href='#'>Resume</a></li>
+                    </ul>
+                </div>
+                <div className='navbar-right'>
+                    <button>Resume</button>
+                </div>
+            </nav>
+        </>
     )
 }
