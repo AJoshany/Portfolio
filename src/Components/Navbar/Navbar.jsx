8a35633b04c7bbb619954ce905f6544d6965879b
Ali Joshany <aa6joshany@gmail.com>
2025-07-13 17:18:15 +0330

    navbar Chenged

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { FiMenu } from "react-icons/fi";
 
 export default function Navbar() {
+    const [showMoblieMenu, setShowMoblieMenu] = useState("")
+    const showMenuHandler = () => {
+        setShowMoblieMenu(prev => (prev === "show" ? "" : "show"));
+    }
     return (
         <>
-            <nav className='navbar-container'>
-                <div className='navbar-left'>
-                    <ul className='navbar-list'>
+            <nav className='navbar-container '>
+                <div className={`navbar-left ${showMoblieMenu}`}>
+                    <div className='navbar-togler'>
+                        <button className='navbar-togler__btn' onClick={() => showMenuHandler()}><FiMenu /></button>
+                    </div>
+                    <ul className={`navbar-list ${showMoblieMenu}`}>
                         <li className="navbar-item active"><a href='#'>Home</a></li>
                         <li className="navbar-item"><a href='#'>Skills</a></li>
                         <li className="navbar-item"><a href='#'>Projects</a></li>
                         <li className="navbar-item"><a href='#'>Resume</a></li>
                     </ul>
+
                 </div>
                 <div className='navbar-right'>
                     <button>Resume</button>
                 </div>
             </nav>
+
         </>
     )
 }
